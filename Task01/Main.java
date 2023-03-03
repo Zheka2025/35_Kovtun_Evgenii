30fb47975875c65f9b3499f8f0e4ed062e4963c4
unknown <Evgenii>
2023-03-03 15:55:03 +0200

    Task01: create simple java program

---
+++ Task01/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Evgenii
+ */
+public class Main {
+public static void main(String[] args) {
+for (String s: args) {
+System.out.println(s);
+}
+}
+}
